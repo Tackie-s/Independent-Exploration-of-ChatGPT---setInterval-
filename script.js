8c5b71c60ec14f4e1bc8a69f3dd12b36ec7bf971
Tackie-s <Tackiesolomon07@gmail.com>
2025-08-15 09:03:49 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// number of seconds that have passed
+let time = 0;
+// reference to the <span> containing the number
+let counterElement = document.getElementById("time");
+
+setInterval(function() {
+    time++;
+    counterElement.textContent = time;
+}, 1000);
